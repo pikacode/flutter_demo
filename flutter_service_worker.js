aa47739141b37087360bcdc1e6f677bc8490b0e9
wuxingchen <pikacode@qq.com>
2025-02-13 10:53:11 +0900

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9eafbc0c7251a6822ce0aed51ad2f40d",
+const RESOURCES = {"flutter_bootstrap.js": "54e31cf08a290bbea26878f835c7d21a",
 "version.json": "6e502247b813c9e8e96d7f7c5f7b90c2",
 "index.html": "0d27ee855935f57258624723a9a15d38",
 "/": "0d27ee855935f57258624723a9a15d38",
-"main.dart.js": "6cef4a7bc1b75e49ed9e6db012f1c5c5",
+"main.dart.js": "d106e2c01071340d1069fed7aef5a164",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
